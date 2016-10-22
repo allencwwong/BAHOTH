4c5859af166221ec378d03f8f0c7cb6be2ca52b8
artcyw <artcyw@gmail.com>
2016-10-22 12:50:38 -0700

    updated char obj

--- js/bahoth.js
+++ js/bahoth.js
@@ -1,4 +1,126 @@
-var chars = [{
-  
-}];
+$(document).ready(function(){
+
+var chars = [
+{
+  "name" : "Brandon Jaspers",
+  "info" : {
+  	"age" : "12",
+  	"height" : "5' 1",
+  	"weight" : "109 lbs",
+  	"hobbies" : "Computers, Camping, Hockey",
+  	"birthday" : "May 21st"	
+  },
+  "mental" : {
+  	"sanity" : 4,
+  	"knowledge" : 3
+  },
+  "physical" : {
+  	"speed" : 4,
+  	"might" : 4
+  }
+},
+{
+  "name" : "Peter Akimoto",
+  "info" : {
+  	"age" : "13",
+  	"height" : "4' 11",
+  	"weight" : "98 lbs",
+  	"hobbies" : "Bugs, Basketball",
+  	"birthday" : "September 3rd"	
+  },
+  "mental" : {
+  	"sanity" : 4,
+  	"knowledge" : 4
+  },
+  "physical" : {
+  	"speed" : 4,
+  	"might" : 3
+  }
+},
+
+{
+  "name" : "Zoe Ingstrom",
+  "info" : {
+  	"age" : "8",
+  	"height" : "3' 9",
+  	"weight" : "49 lbs",
+  	"hobbies" : "Dolls, Music",
+  	"birthday" : "November 5th"	
+  },
+  "mental" : {
+  	"sanity" : 5,
+  	"knowledge" : 3
+  },
+  "physical" : {
+  	"speed" : 4,
+  	"might" : 3
+  }
+},
+
+{
+  "name" : "Missy Dubourde",
+  "info" : {
+  	"age" : "9",
+  	"height" : "4' 2",
+  	"weight" : "62 lbs",
+  	"hobbies" : "Swimming, Medicine",
+  	"birthday" : "February 14th"	
+  },
+  "mental" : {
+  	"sanity" : 3,
+  	"knowledge" : 4
+  },
+  "physical" : {
+  	"speed" : 5,
+  	"might" : 3
+  }
+},
+
+{
+  "name" : "Father Rhinehardt",
+  "info" : {
+  	"age" : "62",
+  	"height" : "5' 9",
+  	"weight" : "185 lbs",
+  	"hobbies" : "Fencing, Gardening",
+  	"birthday" : "April 29th"	
+  },
+  "mental" : {
+  	"sanity" : 6,
+  	"knowledge" : 4
+  },
+  "physical" : {
+  	"speed" : 3,
+  	"might" : 2
+  }
+},
+
+{
+  "name" : "Professor Longfellow",
+  "info" : {
+  	"age" : "57",
+  	"height" : "5' 11",
+  	"weight" : "153 lbs",
+  	"hobbies" : "Gaelic Music, Drama, Fine Wines ",
+  	"birthday" : "July 27th"	
+  },
+  "mental" : {
+  	"sanity" : 3,
+  	"knowledge" : 5
+  },
+  "physical" : {
+  	"speed" : 4,
+  	"might" : 3
+  }
+},
+
+{
+
+}
+];
+console.log(chars[5]);
+});
+console.log("chivkrn tuesday");
+
+
 
