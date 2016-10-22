4c6670a8a1a59a2f92b729e9d92d1fca41bdd0d7
Allen Wong <allencwwong@gmail.com>
2016-10-22 14:57:29 -0700

    updated chars

--- js/bahoth.js
+++ js/bahoth.js
@@ -4,113 +4,227 @@ var chars = [
 {
   "name" : "Brandon Jaspers",
   "info" : {
-  	"age" : "12",
-  	"height" : "5' 1",
-  	"weight" : "109 lbs",
-  	"hobbies" : "Computers, Camping, Hockey",
-  	"birthday" : "May 21st"
+    "age" : "12",
+    "height" : "5' 1",
+    "weight" : "109 lbs",
+    "hobbies" : "Computers, Camping, Hockey",
+    "birthday" : "May 21st" 
   },
   "mental" : {
-  	"sanity" : 4,
-  	"knowledge" : 3
+    "sanity" : 4,
+    "knowledge" : 3
   },
   "physical" : {
-  	"speed" : 4,
-  	"might" : 4
+    "speed" : 4,
+    "might" : 4
   }
 },
 {
   "name" : "Peter Akimoto",
   "info" : {
-  	"age" : "13",
-  	"height" : "4' 11",
-  	"weight" : "98 lbs",
-  	"hobbies" : "Bugs, Basketball",
-  	"birthday" : "September 3rd"
+    "age" : "13",
+    "height" : "4' 11",
+    "weight" : "98 lbs",
+    "hobbies" : "Bugs, Basketball",
+    "birthday" : "September 3rd"  
   },
   "mental" : {
-  	"sanity" : 4,
-  	"knowledge" : 4
+    "sanity" : 4,
+    "knowledge" : 4
   },
   "physical" : {
-  	"speed" : 4,
-  	"might" : 3
+    "speed" : 4,
+    "might" : 3
   }
 },
 
 {
   "name" : "Zoe Ingstrom",
   "info" : {
-  	"age" : "8",
-  	"height" : "3' 9",
-  	"weight" : "49 lbs",
-  	"hobbies" : "Dolls, Music",
-  	"birthday" : "November 5th"
+    "age" : "8",
+    "height" : "3' 9",
+    "weight" : "49 lbs",
+    "hobbies" : "Dolls, Music",
+    "birthday" : "November 5th" 
   },
   "mental" : {
-  	"sanity" : 5,
-  	"knowledge" : 3
+    "sanity" : 5,
+    "knowledge" : 3
   },
   "physical" : {
-  	"speed" : 4,
-  	"might" : 3
+    "speed" : 4,
+    "might" : 3
   }
 },
 
 {
   "name" : "Missy Dubourde",
   "info" : {
-  	"age" : "9",
-  	"height" : "4' 2",
-  	"weight" : "62 lbs",
-  	"hobbies" : "Swimming, Medicine",
-  	"birthday" : "February 14th"
+    "age" : "9",
+    "height" : "4' 2",
+    "weight" : "62 lbs",
+    "hobbies" : "Swimming, Medicine",
+    "birthday" : "February 14th"  
   },
   "mental" : {
-  	"sanity" : 3,
-  	"knowledge" : 4
+    "sanity" : 3,
+    "knowledge" : 4
   },
   "physical" : {
-  	"speed" : 5,
-  	"might" : 3
+    "speed" : 5,
+    "might" : 3
   }
 },
 
 {
   "name" : "Father Rhinehardt",
   "info" : {
-  	"age" : "62",
-  	"height" : "5' 9",
-  	"weight" : "185 lbs",
-  	"hobbies" : "Fencing, Gardening",
-  	"birthday" : "April 29th"
+    "age" : "62",
+    "height" : "5' 9",
+    "weight" : "185 lbs",
+    "hobbies" : "Fencing, Gardening",
+    "birthday" : "April 29th" 
   },
   "mental" : {
-  	"sanity" : 6,
-  	"knowledge" : 4
+    "sanity" : 6,
+    "knowledge" : 4
   },
   "physical" : {
-  	"speed" : 3,
-  	"might" : 2
+    "speed" : 3,
+    "might" : 2
   }
 },
 
 {
   "name" : "Professor Longfellow",
   "info" : {
-  	"age" : "57",
-  	"height" : "5' 11",
-  	"weight" : "153 lbs",
-  	"hobbies" : "Gaelic Music, Drama, Fine Wines ",
-  	"birthday" : "July 27th"
+    "age" : "57",
+    "height" : "5' 11",
+    "weight" : "153 lbs",
+    "hobbies" : "Gaelic Music, Drama, Fine Wines ",
+    "birthday" : "July 27th"  
   },
   "mental" : {
-  	"sanity" : 3,
-  	"knowledge" : 5
+    "sanity" : 3,
+    "knowledge" : 5
   },
   "physical" : {
-  	"speed" : 4,
-  	"might" : 3
+    "speed" : 4,
+    "might" : 3
+  }
+},
+
+{
+  "name" : "Jenny LeClerc",
+  "info" : {
+    "age" : "21",
+    "height" : "5' 7",
+    "weight" : "142 lbs",
+    "hobbies" : "Reading, Soccer",
+    "birthday" : "March 4th"  
+  },
+  "mental" : {
+    "sanity" : 4,
+    "knowledge" : 3
+  },
+  "physical" : {
+    "speed" : 4,
+    "might" : 4
+  }
+},
+
+{
+  "name" : "Heather Granville",
+  "info" : {
+    "age" : "18",
+    "height" : "5' 2",
+    "weight" : "120 lbs",
+    "hobbies" : "Television, Shopping",
+    "birthday" : "Auguest 2nd"  
+  },
+  "mental" : {
+    "sanity" : 3,
+    "knowledge" : 5
+  },
+  "physical" : {
+    "speed" : 4,
+    "might" : 3
+  }
+},
+
+{
+  "name" : "Ox Bellows",
+  "info" : {
+    "age" : "23",
+    "height" : "6' 4",
+    "weight" : "288 lbs",
+    "hobbies" : "Football, Shiny Objects",
+    "birthday" : "October 18th"  
+  },
+  "mental" : {
+    "sanity" : 3,
+    "knowledge" : 3
+  },
+  "physical" : {
+    "speed" : 4,
+    "might" : 5
+  }
+},
+
+{
+  "name" : "Darrin \"Flash\" Williams",
+  "info" : {
+    "age" : "20",
+    "height" : "5' 11",
+    "weight" : "188 lbs",
+    "hobbies" : "Track, Music, Shakespearean Literature",
+    "birthday" : "June 6th"  
+  },
+  "mental" : {
+    "sanity" : 3,
+    "knowledge" : 3
+  },
+  "physical" : {
+    "speed" : 6,
+    "might" : 3
+  }
+},
+
+{
+  "name" : "Madame Zostra",
+  "info" : {
+    "age" : "37",
+    "height" : "5' 0",
+    "weight" : "150 lbs",
+    "hobbies" : "Astrology, Cooking, Baseball",
+    "birthday" : "December 10th"  
+  },
+  "mental" : {
+    "sanity" : 4,
+    "knowledge" : 4
+  },
+  "physical" : {
+    "speed" : 3,
+    "might" : 4
+  }
+},
+
+{
+  "name" : "Vivian Lopez",
+  "info" : {
+    "age" : "42",
+    "height" : "5' 5",
+    "weight" : "142 lbs",
+    "hobbies" : "Old Movies, Horses",
+    "birthday" : "January 11th"  
+  },
+  "mental" : {
+    "sanity" : 4,
+    "knowledge" : 5
+  },
+  "physical" : {
+    "speed" : 4,
+    "might" : 2
   }
 },
 
@@ -118,6 +232,9 @@ var chars = [
 
 }
 ];
-console.log(chars[5]);
+console.log(chars[9]);
 });
 console.log("chivkrn tuesday");
+
+
+
